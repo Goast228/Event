cfc6aea5e1a0271fea7c13b44546d8b6e3dad128
Goast228 <91661766+Goast228@users.noreply.github.com>
2022-03-23 19:08:39 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public class WorkerEventArgs : EventArgs
+        {
+            public char value { get; }
+            public WorkerEventArgs(char Value)
+            {
+                value = Value;
+            }
+        }
+        public class Worker
+        {
+            public event EventHandler<char> OnKeyPresed;
+
+            public void Run()
+            {
+                while (true)
+                {
+                    Console.WriteLine("Введите символ: ");
+                    var symbol = Console.ReadKey();
+                    if (symbol.Key == ConsoleKey.C)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        OnKeyPresed?.Invoke(this, symbol.KeyChar);
+                    }
+
+                }
+            }
+        }
+        public class Subscriber
+        {
+            public void Method()
+            {
+                var worker = new Worker();
+                worker.OnKeyPresed += Print;
+                worker.Run();
+            }
+            public void Print(object sender, char e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Subscriber subscriber = new Subscriber();
+            subscriber.Method();
+
+        }
+    }
+}
+
